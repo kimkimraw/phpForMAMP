8daef682340c72ebb3db08c7140de31ee261830a
sou kimura <nattoukimuti0201@gmail.com>
2021-05-30 16:25:17 +0900

    共通関数フォルダ追加　common

--- limited/year.php
+++ limited/year.php
@@ -16,32 +16,11 @@
 	
 	<main>
 		<?php
+			require_once('../common/common.php');
+
 			$year = $_POST['year'];
-
 			$wareki = gengo($year);
-			print $wareki;
-
-			function gengo($val){
-				$gengo = '入力内容に対応しておりません。別の値を入力してやり直してください。';
-
-				if(1868 <= $val && 1911 >= $val){
-					$gengo = '明治';
-				}
-				if(1912 <= $val && 1925 >= $val){
-					$gengo = '大正';
-				}
-				if(1926 <= $val && 1988 >= $val){
-					$gengo = '昭和';
-				}
-				if(1989 <= $val && 2018 >= $val){
-					$gengo = '平成';
-				}
-				if(2019 <= $val){
-					$gengo = '令和';
-				}
-				return $gengo;
-			}
-		
+			print $wareki;		
 		?>
 	</main>
 
